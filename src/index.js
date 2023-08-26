bb355a4d0754ee03adeac7e5a95f7b0a8d0b3e72
ZhiShengYuan <37360003+ZhiShengYuan@users.noreply.github.com>
2023-08-26 23:56:30 +0800

    add key

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ const PostalMime = require('postal-mime');
 
 // Define a list of email addresses and their corresponding chatIds and forward email ids
       const emailList = [
-        { to: "example@gmail.com", chatId: 5071059420, fMailid: "example@example.com", toForward: false }, //only sends notification as its false
-        { to: "example1@example.com", chatId: 5071059420, fMailid: "example3@example.com", toForward: true }, //sends & forward
-        { to: "example2@example.com", chatId: 5071059420, fMailid: "example4@example.com", toForward: false },
+        { to: "*", chatId: 1878878763, fMailid: "*", toForward: true }, //sends & forward
       ];
 
 async function streamToArrayBuffer(stream, streamSize) {
